569c0ddb64ffde84180e577f0d84179045b68be2
jkv27100 <jagannathkvjkv@gmail.com>
2021-06-02 13:24:27 +0530

    routes

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+import debug from "debug";
 import express from "express";
+import auth from "./routes/auth.js";
 
 const app = express();
+const devDebug = debug("app:dev");
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+app.use("/auth", auth.router);
+
+devDebug(app.get("env"));
+const PORT = process.env.PORT || 4500;
+app.listen(PORT, () => {
+  devDebug("server running" + PORT);
 });
