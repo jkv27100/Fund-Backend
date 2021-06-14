b2f9b8fbfc9e32eee055023d7e236741395980c1
abiaugus <appus525@gmail.com>
2021-06-14 08:04:29 +0530

    post model

---
+++ models/post.js
@@ -0,0 +1,74 @@
+const mongoose = require(mongoose);
+
+const Schema = new mongoose.Schema();
+
+const goalSchema = new Schema({
+    goalAmount: {
+        type: Number,
+        required: true
+    },
+    goalDays: {
+        type: Number,
+        required: true
+    },
+    amountRaised: {
+        type: Number,
+        required: true
+    }
+});
+
+
+const socialInteractionsSchema = new Schema({
+    upvotes: {
+        type: Number
+    },
+    downvotes: {
+        type: Number
+    },
+    comments: {
+        type: [String]
+    }
+})
+
+const postSchema = new Schema({
+    postId: {
+        type: Number,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true,
+        default: null // add stock image link
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    subtitle: {
+        type: String,
+        required: true
+    },
+    postId: {
+        type: Number,
+        required: true
+    },
+
+    tags: {
+        type: [String]
+    },
+    percentage: {
+        type: Number,
+        required: true
+    },
+    isApproved: {
+        type: Boolean,
+        default: false
+    },
+    socialInteractions : socialInteractionsSchema,
+    goalDetails : goalSchema
+    
+},{timestamps : true})
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
