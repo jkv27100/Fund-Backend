d0bb301036b6bd488f85e6dcbfa1474a2307bfdf
jkv27100 <jagannathkvjkv@gmail.com>
2021-06-14 10:36:28 +0530

    looks good

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    
-})
+  //rasengan
+});
--- routes/notifications.routes.js
+++ routes/notifications.routes.js
@@ -1,14 +1,13 @@
 const router = require("express").Router();
-const notificationController = require('../controllers/notification.controller');
-
+const notificationController = require("../controllers/notification.controller");
+//or const {getNotification,getNotificationById..} = require(...path)
 router.get("/", (req, res) => {
-    res.send("Noti root working")
-})
+  res.send("Noti root working");
+});
 
-router.get("/get-noti",notificationController.getNotification);
-router.get("/get-noti/:user_id",notificationController.getNotificationById);
-router.post("/add-noti",notificationController.addNotification);
-router.get("/delete-noti",notificationController.deleteNotification);
+router.get("/get-noti", notificationController.getNotification);
+router.get("/get-noti/:user_id", notificationController.getNotificationById);
+router.post("/add-noti", notificationController.addNotification);
+router.get("/delete-noti", notificationController.deleteNotification);
 
 module.exports = router;
-
