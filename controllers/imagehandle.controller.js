299f1390b5f07b2fb6bfa8846111d3808686f22d
jkv27100 <jagannathkvjkv@gmail.com>
2021-07-05 12:51:04 +0530

    chnage uploaded image fucntion is added

--- controllers/imagehandle.controller.js
+++ controllers/imagehandle.controller.js
@@ -2,9 +2,16 @@ const Profile = require("../models/userProfile");
 const log = require("debug")("app:dev");
 
 const uploadImage = async (req, res) => {
+  const user_id = req.body.user_id;
+  let profile_img = req.file.buffer;
+  const result = await Profile.findOneAndUpdate({ user_id }, { profile_img });
+
+  if (result)
+    return res.status(200).send({ success: true, message: "Profile Updated" });
+
   const data = {
-    user_id: req.body.user_id,
-    profile_img: req.file.buffer,
+    user_id,
+    profile_img,
   };
   const userProfile = await Profile.create(data);
   res.status(200).send({ success: true, message: "Profile Photo uploaded!" });
